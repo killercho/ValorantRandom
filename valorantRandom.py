ff668305019306f4f61ee4edc0a2d61604a616db
killercho <ggeorgi60@gmail.com>
2023-08-25 20:54:57 +0300

    Added dicts with all buyable options

--- valorantRandom.py
+++ valorantRandom.py
@@ -10,12 +10,21 @@ parser.add_argument("-r", "--rules", action="store_true",  dest="rules",
 parser.add_argument("-a", "--agent", action="store_true",  dest="agent",
                     default=0, help="shows a random agent pick instead of the normal weapons")
 
+parser.add_argument("-c", "--continuous", action="store_true",  dest="continuous",
+                    default=0, help="runs the program continuously and recieves input")
+
 args = parser.parse_args()
 
-guns = ["None", "Stinger", "Spectre", "Bucky", "Judge", "Buldog",
-        "Guardian", "Phantom", "Vandal", "Marshal", "Operator", "Ares", "Odin"]
-pistols = ["None", "Classic", "Shorty", "Frenzy", "Ghost", "Sheriff"]
-armor = ["None", "Small", "Big"]
+# Idea: the random choice has the price of the item that is going to be used to calculate the max value and whether the player can buy it.
+#       If the player does not have enough money for something from this dict remove all options that exceed the value given.
+
+guns = {"None": 0, "Stinger": 1100, "Spectre": 1600, "Bucky": 850, "Judge": 1800, "Buldog": 2050,
+        "Guardian": 2100, "Phantom": 2900, "Vandal": 2900, "Marshal": 950, "Operator": 4700, "Ares": 0, "Odin": 3200}
+
+pistols = {"None": 0, "Shorty": 300, "Frenzy": 450, "Ghost": 500, "Sheriff": 800}
+
+armor = {"None": 0, "Small": 400, "Big": 1000}
+
 agents = ["Astra", "Breach", "Brimstone", "Deadlock", "Chamber", "Cypher", "Fade", "Gekko", "Jett", "Kay/O", "Killjoy",
           "Neon", "Omen", "Harbor", "Phoenix", "Raze", "Reyna", "Sage", "Skye", "Sova", "Viper", "Yoru"]
 
@@ -31,4 +40,4 @@ elif args.rules:
 else:
     result = "Agent: " + random.choice(agents)
 
-print(result)
+# print(result)
